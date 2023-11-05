7b107549820a9736714ecf1316d3088b79505b60
Omar-Emam-99 <omaressamemam22@gmail.com>
2023-11-05 15:32:33 +0000

    Update` AwareLoss Model`

--- models/ClassifierAwareLoss.py
+++ models/ClassifierAwareLoss.py
@@ -1,18 +1,15 @@
-import sys
-from pathlib import Path
-src_dir = Path.cwd().parent
-sys.path.append(str(src_dir))
-from utilits.utilits import *
-import torch
+"""Use AwareLoss function with Classifier Model"""
 import os
-from tqdm.auto import tqdm
+import torch
 import evaluate
 from torch import nn
-import torch.nn.functional as F
+from tqdm.auto import tqdm
+from utilits.utilits import *
 from torch.optim import AdamW
+import torch.nn.functional as F
 from collections import defaultdict
-from transformers import get_scheduler, AutoConfig
 from torch.utils.data import DataLoader
+from transformers import get_scheduler, AutoConfig
 from transformers import Trainer, TrainingArguments
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
 from models.NoiseAwareLoss import (
@@ -28,7 +25,7 @@ class ClassifierAwareLoss:
     ClassifierAwareLoss is a class that performs training using a sequence classification model and a custom loss function.
     """
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
 
     def __init__(self, model_ckpt: str = "distilbert-base-uncased"):
         """
@@ -38,8 +35,9 @@ class ClassifierAwareLoss:
         - model_ckpt: The checkpoint name or path for the pre-trained model.
 
         """
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_ckpt, num_labels=2).to(device)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_ckpt, num_labels=2).to(self.device)
 
     def tokenize(self, batch):
         """
@@ -47,10 +45,9 @@ class ClassifierAwareLoss:
 
         Parameters:
         - batch: a dictionary representing a batch of input data
-
+        
         Returns:
         - a dictionary containing the tokenized inputs
-
         """
         return self.tokenizer(batch["tweets"], padding="max_length", truncation=True)
 
@@ -66,16 +63,17 @@ class ClassifierAwareLoss:
         - eval_dataloader: The DataLoader for the evaluation dataset.
 
         """
-        tokenized_dataset = train_data.map(self.tokenize, batched=True)
-        tokenized_dataset = tokenized_dataset.remove_columns(["tweets"])
+        tokenized_dataset = data.map(self.tokenize , batched=True)
+        tokenized_dataset = tokenized_dataset.remove_columns(["tweets" , "labels_name"])
         tokenized_dataset.set_format('torch')
+        
+        return tokenized_dataset
 
-        train_dataloader = DataLoader(tokenized_dataset["train"], shuffle=True, batch_size=16)
-        eval_dataloader = DataLoader(tokenized_dataset["test"], shuffle=True, batch_size=16)
-
-        return train_dataloader, eval_dataloader
-
-    def train(self, dataset, num_epochs: int = 5, learning_rate: float = 2e-5, weight_decay: float = 0.001):
+    def train(self,
+              dataset,
+              num_epochs:int=2,
+              learning_rate:float=2e-5,
+              weight_decay:float=0.001):
         """
         Trains the model using the provided dataset.
 
@@ -84,10 +82,11 @@ class ClassifierAwareLoss:
         - num_epochs: The number of training epochs.
         - learning_rate: The learning rate for the optimizer.
         - weight_decay: The weight decay for the optimizer.
-
         """
         # Get data loaders
-        train_dataloader, eval_dataloader = self.prepare_data(dataset)
+        tokenized_dataset = self.prepare_data(dataset)
+        train_dataloader = DataLoader(tokenized_dataset["train"], shuffle=True, batch_size=16)
+        eval_dataloader = DataLoader(tokenized_dataset["test"], shuffle=True, batch_size=16)
 
         num_training_steps = num_epochs * len(train_dataloader)
         progress_bar = tqdm(range(num_training_steps), colour="green")
@@ -97,12 +96,12 @@ class ClassifierAwareLoss:
         optimizer = AdamW(self.model.parameters(), lr=learning_rate)
         LR_Scheduler = get_scheduler(name='linear', optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 
-        self.model.train()
+        
         for epoch in range(num_epochs):
+            self.model.train()
             for batch in train_dataloader:
-                batch = {k: v.to(device) for k, v in batch.items()}
+                batch = {k: v.to(self.device) for k, v in batch.items()}
                 outputs = self.model(**batch)
-
                 loss_fct = NCEandMAE(1, 1, 2)
                 loss = loss_fct(outputs.logits, batch["labels"])
                 loss.backward()
@@ -110,15 +109,44 @@ class ClassifierAwareLoss:
                 LR_Scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
-
+            print(f"train-Loss : {loss}")
+            
             self.model.eval()
             for batch in eval_dataloader:
-                batch = {k: v.to(device) for k, v in batch.items()}
+                batch = {k: v.to(self.device) for k, v in batch.items()}
                 with torch.no_grad():
                     outputs = self.model(**batch)
-
+                valid_loss = outputs.loss
                 logits = outputs.logits
                 predictions = torch.argmax(logits, dim=-1)
                 metric.add_batch(predictions=predictions, references=batch["labels"])
+                progress_bar.update(1)
+            print(f"Valid-Loss : {valid_loss}")
 
-        metric.compute()
+        print(metric.compute())
+
+    def test(self,test_data):
+        """
+        Tests the trained model on test data and returns classification metrics.
+        
+        Parameters:
+        - test_data: a dataset object representing the test data
+
+        Returns:
+        - a dictionary containing the classification metrics
+
+        """
+        metric = evaluate.combine(["accuracy", "f1", "precision", "recall"])
+        test_encoded = self.prepare_data(test_data)
+        test_dataloader = DataLoader(test_encoded, batch_size=16)
+        
+        self.model.eval()
+        for batch in test_dataloader:
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+            with torch.no_grad():
+                outputs = self.model(**batch)
+            logits = outputs.logits
+            predictions = torch.argmax(logits, dim=-1)
+            metric.add_batch(predictions=predictions, references=batch["labels"])
+        
+        return metric.compute()
