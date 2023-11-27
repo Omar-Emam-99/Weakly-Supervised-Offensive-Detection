ba9ef05cc63553939b0afcfb1c323b4497636ce4
Omar-Emam-99 <omaressamemam22@gmail.com>
2023-11-27 18:21:51 +0000

    Reomve methods

--- models/dual_contrastive_model/model.py
+++ models/dual_contrastive_model/model.py
@@ -6,7 +6,7 @@ class Transformer(nn.Module):
     Transformer class that is a child of the nn.Module class in PyTorch.
     """
 
-    def __init__(self, base_model, num_classes, method):
+    def __init__(self, base_model, num_classes):
         """
         Initialize the Transformer class with a base model, number of classes, and a method.
 
@@ -18,7 +18,6 @@ class Transformer(nn.Module):
         super().__init__()
         self.base_model = base_model
         self.num_classes = num_classes
-        self.method = method
         self.linear = nn.Linear(base_model.config.hidden_size, num_classes)
         self.dropout = nn.Dropout(0.5)
 
@@ -46,13 +45,14 @@ class Transformer(nn.Module):
         cls_feats = hiddens[:, 0, :]
 
         # Depending on the method, set label features and calculate predicts
+        """
         if self.method in ['ce', 'scl']:
-            label_feats = None
-            predicts = self.linear(self.dropout(cls_feats))
+        label_feats = None
+        predicts = self.linear(self.dropout(cls_feats))
         else:
-            label_feats = hiddens[:, 1:self.num_classes+1, :]
-            predicts = torch.einsum('bd,bcd->bc', cls_feats, label_feats)
-
+        """
+        label_feats = hiddens[:, 1:self.num_classes+1, :]
+        predicts = torch.einsum('bd,bcd->bc', cls_feats, label_feats)
         # Return a dictionary containing the predicted outputs, class features, and label features
         outputs = {
             'predicts': predicts,
